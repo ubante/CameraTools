fa228f0ee8a07b80ee49d6efd8436bd84b2ceaa2
John Ubante <ubante@yahoo-inc.com>
2014-02-09 01:19:33 -0800

    more stuff

---
+++ CameraTools/src/main/java/com/ubante/cameratools/models/Canon5D3.java
@@ -0,0 +1,8 @@
+package com.ubante.cameratools.models;
+
+/**
+ * Created by Con_0 on 2/9/14.
+ */
+public class Canon5D3 extends CameraBody {
+    Float rawFileSize = 26.8f; // in MB
+}
---
+++ CameraTools/src/main/java/com/ubante/cameratools/models/Canon60D.java
@@ -0,0 +1,8 @@
+package com.ubante.cameratools.models;
+
+/**
+ * Created by Con_0 on 2/9/14.
+ */
+public class Canon60D extends CameraBody {
+    Float rawFileSize = 22.5f; // in MB
+}
